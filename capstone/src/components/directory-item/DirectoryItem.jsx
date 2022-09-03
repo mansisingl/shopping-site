53f034bcec73c202227aef73efb457d32c4a326b
champak kumar <100565945+singlas106@users.noreply.github.com>
2022-09-03 14:01:19 +0530

    category item to directory item to resolve conflicting CSS

--- capstone/src/components/directory-item/DirectoryItem.jsx
+++ capstone/src/components/directory-item/DirectoryItem.jsx
@@ -3,12 +3,12 @@ import './DirectoryItem.scss';
 
 const DirectoryItem = ({category: {title,imageUrl}}) => {
   return (
-    <div className="category-container">
+    <div className="directory-item-container">
         <div className='background-image' style={{
         backgroundImage: `url(${imageUrl})`
         }}
         />
-        <div className="category-body-container">
+        <div className="body">
             <h2>{title}</h2>
             <p>Shop Now</p>
         </div>
